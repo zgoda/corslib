7a6b6653f7c6cb5785ea1e4249fcd4d3581d84d2
Jarek Zgoda <jarek.zgoda@gmail.com>
2021-03-23 22:25:57 +0100

    Tests for allow_credentials setup

--- tests/test_policy.py
+++ tests/test_policy.py
@@ -1,4 +1,5 @@
-from corslib.policy import Policy
+import pytest
+from corslib.policy import Policy, OriginRule, PolicyError
 
 
 def test_default_create():
@@ -7,3 +8,19 @@ def test_default_create():
     assert not any(
         [p.allow_origin, p.allow_headers, p.allow_methods, p.expose_headers, p.max_age]
     )
+
+
+def test_allow_credentials_setup_valid():
+    p = Policy(
+        name='policy1',
+        allow_credentials=True, allow_origin=[OriginRule(rule='website.com')],
+    )
+    assert p
+
+
+@pytest.mark.parametrize(
+    'origin', [None, [OriginRule(rule='*')]], ids=['empty', 'star']
+)
+def test_allow_credentials_setup_invalid_empty_origin(origin):
+    with pytest.raises(PolicyError):
+        Policy(name='policy1', allow_credentials=True, allow_origin=origin)
